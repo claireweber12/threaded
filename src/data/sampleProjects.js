7aa06a539e7ad3b3c5953c46fb00e9db76281380
claireweber12 <claire.weber2004@icloud.com>
2026-05-07 19:30:21 -0500

    add sample data

---
+++ src/data/sampleProjects.js
@@ -0,0 +1,82 @@
+
+const sampleProjects = [
+    {
+        id:1,
+        title: "Don't Be Dull",
+        status: "In Progress",
+        designer: "Lycette Designs",
+        tags: ["sayings", "beginner"],
+        threads:[
+            {
+                brand: "Silk and Ivory",
+                colorNumber: "147",
+                colorName:'Carnation',
+                colorHex:"#F9629F",
+            },
+            {
+                brand:"Silk and Ivory",
+                colorNumber:"149",
+                colorName:"Azalea",
+                colorHex:"#F06292",
+            },
+            {
+                brand:"Silk and Ivory",
+                colorNumber:"002",
+                colorName:"White",
+                colorHex:"#ffffff",
+            }
+        ]
+    },
+    {
+        id:2,
+        title: "Toomer's Corner Tree Auburn",
+        status: "Completed",
+        designer: "Sunday Shoppe",
+        tags: ["ornament", "round", "college"],
+        threads:[
+            {
+                brand: "Pepper Pot Silk",
+                colorNumber: "002",
+                colorName:'Salt',
+                colorHex:"#ffffff",
+            },
+            {
+                brand:"Pepper Pot Silk",
+                colorNumber:"107",
+                colorName:"Ceramic",
+                colorHex:'#9ccfe9',
+            },
+            {
+                brand:"Essentials",
+                colorNumber:"717",
+                colorName:"Appletini",
+                colorHex:"#e3f8a9",
+            },
+            {
+                brand:"Essentials",
+                colorNumber:"627",
+                colorName:"Chipmunk",
+                colorHex:"#a2865f",
+            },
+            {
+                brand:"Essentials",
+                colorNumber:"727",
+                colorName:"Eucalyptus",
+                colorHex:"#819d6a",
+            },
+            {
+                brand:"Essentials",
+                colorNumber:"853",
+                colorName:"Western",
+                colorHex:"#625220",
+            },
+            {
+                brand:"Petite Very Velvet",
+                colorNumber:"652",
+                colorName:"Hunter Green",
+                colorHex:"#4a8c4f",
+            },
+        ]
+    }
+]
+export default sampleProjects;
