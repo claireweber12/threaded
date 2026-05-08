9f017f5d705f7c1236a104ec8f716cbe001d09f6
claireweber12 <claire.weber2004@icloud.com>
2026-05-08 18:17:21 -0500

    add new project form

--- src/data/sampleProjects.js
+++ src/data/sampleProjects.js
@@ -14,18 +14,21 @@ const sampleProjects = [
                 colorNumber: "147",
                 colorName:'Carnation',
                 colorHex:"#F9629F",
+                numSkeins: '1',
             },
             {
                 brand:"Silk and Ivory",
                 colorNumber:"149",
                 colorName:"Azalea",
                 colorHex:"#F06292",
+                numSkeins: '1',
             },
             {
                 brand:"Silk and Ivory",
                 colorNumber:"002",
                 colorName:"White",
                 colorHex:"#ffffff",
+                numSkeins: '1',
             }
         ]
     },
@@ -43,42 +46,49 @@ const sampleProjects = [
                 colorNumber: "002",
                 colorName:'Salt',
                 colorHex:"#ffffff",
+                numSkeins: '1',
             },
             {
                 brand:"Pepper Pot Silk",
                 colorNumber:"107",
                 colorName:"Ceramic",
                 colorHex:'#9ccfe9',
+                numSkeins: '1',
             },
             {
                 brand:"Essentials",
                 colorNumber:"717",
                 colorName:"Appletini",
                 colorHex:"#e3f8a9",
+                numSkeins: '1',
             },
             {
                 brand:"Essentials",
                 colorNumber:"627",
                 colorName:"Chipmunk",
                 colorHex:"#a2865f",
+                numSkeins: '1',
             },
             {
                 brand:"Essentials",
                 colorNumber:"727",
                 colorName:"Eucalyptus",
                 colorHex:"#819d6a",
+                numSkeins: '1',
             },
             {
                 brand:"Essentials",
                 colorNumber:"853",
                 colorName:"Western",
                 colorHex:"#625220",
+                numSkeins: '1',
             },
             {
                 brand:"Petite Very Velvet",
                 colorNumber:"652",
                 colorName:"Hunter Green",
                 colorHex:"#4a8c4f",
+                numSkeins: '1',
             },
         ]
     }
--- src/pages/NewProject.jsx
+++ src/pages/NewProject.jsx
@@ -1,10 +1,67 @@
 
 function NewProject() {
     return(
-        <div className='page new-project'>
-            <h1>New Project</h1>
-            <h2>Add a new project to your tracker</h2>
-        </div>
+        <main className='new-project-page'>
+            <h1>Add New Project!</h1>
+            <p>Start documenting a new project</p>
+            <div className='form-card'>
+                <h3>Project Details:</h3>
+                <div className='form-section'>
+                    <div className='form-row'>
+                        <div className='form-group'>
+                            <label htmlFor='title'>Title</label>
+                            <input id='title' type='text'/>
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='designer'>Designer</label>
+                            <input id='designer' type='text'/>
+                        </div>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='status'>Project Status</label>
+                        <select id='status'>
+                            <option value='planned'>Planned</option>
+                            <option value='started'>Started</option>
+                            <option value='completed'>Completed</option>
+                            <option value='paused'>Paused</option>
+                            <option value='abandoned'>Abandoned</option>
+                        </select>
+                    </div>
+                </div>
+                <h3>Threads Used:</h3>
+                <div className='form-section'> 
+                    <div className='form-row'>
+                        <div className='form-group'>
+                            <label htmlFor='brand'>Brand</label>
+                            <input id='brand' type='text'/>
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='number'>Number</label>
+                            <input id='number' type='text'/>
+                        </div>
+                    </div>
+                    <div className='form-row'>
+                        <div className='form-group'>
+                            <label htmlFor='color-name'>Color Name</label>
+                            <input id='color-name' type='text' />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='num-skeins'>Number of Skeins</label>
+                            <input id='num-skiens' type='text'/>
+                        </div>
+                    </div>
+                </div>
+                <div className='form-section'>
+                    <div className='form-group'>
+                        <label htmlFor='text-area'>Notes:</label>
+                        <textarea id='text-area'></textarea>
+                    </div>
+                </div>
+
+                <button id='create-btn'>Create Project</button>
+
+            </div>
+        </main>
         
     )
 }
